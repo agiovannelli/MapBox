c05e44189b7b283482924664ee2fc6da0d7dd501
agiovannelli <agiovannelli2234@gmail.com>
2016-01-21 22:18:40 -0500

    Initial commit Added Navigation action bar

---
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/NavigationDialogTestFragment.java
@@ -0,0 +1,9 @@
+package com.mapbox.mapboxsdk.android.testapp;
+
+/**
+ * Created by Alex on 1/21/16.
+ */
+public class NavigationDialogTestFragment extends android.support.v4.app.Fragment
+{
+    // Go to res -> layout and add new fragment_navigationdialog
+}
